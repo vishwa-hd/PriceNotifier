982bc5fcc5114b370e01d1ebd8c586de5b1dc430
vishwa-hd <vishwahd041@gmail.com>
2024-06-23 20:44:56 +0530

    Updated connection logic and increased timeout

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,21 +1,22 @@
 import mongoose from 'mongoose';
 
-let isConnected = false;// variable to track the connection status
+let isConnected = false; // variable to track the connection status
 
 export const connectToDB = async () => {
-  mongoose.set('strictQuery',true);
-  
-  if(!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
+  mongoose.set('strictQuery', true);
 
-  if(isConnected) return console.log('=> using existing database connection');
+  if (!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
+  if (isConnected) return console.log('=> using existing database connection');
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+     
+      serverSelectionTimeoutMS: 3000, // Increase timeout to 30 seconds
+    });
 
     isConnected = true;
-
     console.log('MongoDB Connected');
-  }catch (error){
-   console.log(error)
+  } catch (error) {
+    console.log(error);
   }
-}
+};
